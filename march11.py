2ae3c5b2c5e9067fd58cb380f9292b0615246518
Purva0417 <purvab269@stemcollegiate.ca>
2025-03-11 14:07:33 -0600

    Create march11.py

---
+++ march11.py
@@ -0,0 +1,18 @@
+#replit day 5
+#Today i learned...
+print("Which Harry Potter characters are you?")
+print("--")
+lazy = input("Are you lazy?")
+if lazy == "yes":
+  print("You are not Hermione Granger")
+else:
+  print("You are Hermione Granger! Congrats!")
+print("--")
+funny = input("Do you make others laugh unintentionally?")
+if funny == "yes":
+    print("You are Ron Weasley!")
+else:
+    print("Do you even know who you are!?")
+print("--")
+print("Anyways...")
+print("YOU ARE HARRY POTTER! YOU ARE THE BEST!")
