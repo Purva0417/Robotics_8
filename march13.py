70701f9613823506a655a378e74950c74d018f6d
Purva0417 <purvab269@stemcollegiate.ca>
2025-03-13 09:06:30 -0600

    Create march13.py

---
+++ march13.py
@@ -0,0 +1,13 @@
+#replit day 6
+#Today i learned...
+print("LOGIN PROGRAM")
+username = input("Enter your username:")
+password = input("Enter your password:")
+if password == "STEMStudent" and username == "Purva":
+  print("Hey Purva! Welcome to the STEM Student Portal!")
+elif password == "PurPleGreeN" and username == "Priyanka":
+  print("Congrats! You have successfully logged in as Priyanka!")
+elif password == "IaMfoRgeTfuL" and username == "Student":
+  print("CONGRATULATIONS! You have finally logged in for once!")
+else:
+  print("Who are you? Go away!")
