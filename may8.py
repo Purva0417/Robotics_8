5dcd0bb33ad0385d67d2864ff94b4d7854aefebc
Purva0417 <purvab269@stemcollegiate.ca>
2025-05-08 09:20:47 -0600

    Create may8.py

---
+++ may8.py
@@ -0,0 +1,17 @@
+# replit day 7
+# Today I learned...
+print("Hello! Welcome to: HOW BIG OF A PAW PATROL FAN ARE YOU?")
+
+Leader = input("Who is the leader of the group?")
+if Leader == "Ryder":
+  print("Yes. You got it right!")
+else: 
+  print("What are you talking about?")
+
+Recycling = input("What is the name of the pup who likes recycling?")
+if Recycling == "Rocky":
+  print("Correct! You are a true fan!")
+else: 
+  print("Do you really want to throw hands?")
+
+print("Do you think you are a true fan?")
